b360a6163e2eec66be5419d8da22953e93998e27
dejager <dejager@gmail.com>
2021-04-28 15:29:49 -0700

    Releasing latest framework binary

--- Mbark.xcframework/ios-arm64/Mbark.framework/Headers/Mbark.h
+++ Mbark.xcframework/ios-arm64/Mbark.framework/Headers/Mbark.h
@@ -11,6 +11,8 @@
 #import <CoreTelephony/CTCarrier.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #include <ifaddrs.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
 
 //! Project version number for mbark.
 FOUNDATION_EXPORT double mbarkVersionNumber;
@@ -20,4 +22,9 @@ FOUNDATION_EXPORT const unsigned char mbarkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <mbark/PublicHeader.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+extern const IMP _mbark_objc_msgForward;
+
+NS_ASSUME_NONNULL_END
 
