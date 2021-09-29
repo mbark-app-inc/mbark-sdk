dcd60eecaf62cbba3ff6c9063f0374a2d1e3a962
dejager <dejager@gmail.com>
2021-09-29 14:17:30 -0700

    Correcting swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,12 +1,7 @@
-//
-//  Package.swift
-//  
-//
-//  Created by Nate de Jager on 2021-09-29.
-//
-
 // swift-tools-version:5.3
+
 import PackageDescription
+
 let package = Package(name: "Mbark",
                       platforms: [
                         .iOS(.v12)
